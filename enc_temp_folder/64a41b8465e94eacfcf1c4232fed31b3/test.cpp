004485d82839e46c13e341f7ab3473c14cfb08e9
wlkkt <yangyf0618@qq.com>
2024-04-17 23:37:46 +0800

    更新

--- enc_temp_folder/64a41b8465e94eacfcf1c4232fed31b3/test.cpp
+++ enc_temp_folder/64a41b8465e94eacfcf1c4232fed31b3/test.cpp
@@ -296,78 +296,41 @@
 //}
 
 /////////////////////////////////////////////////////////复杂的菱形继承和菱形虚拟继承
-//#include <iostream>
-//#include <string>
-//using namespace std;
-//
-//class Person
-//{
-//public:
-//	string _name; // 姓名
-//};
-//
-//class Student : virtual public Person
-//{
-//protected:
-//	int _num; //学号
-//};
-//
-//class Teacher : virtual public Person
-//{
-//protected:
-//	int _id; // 职工编号
-//};
-//
-//class Assistant : public Student, public Teacher
-//{
-//protected:
-//	string _majorCourse; // 主修课程
-//};
-//
-//int main()
-//{
-//	// 这样会有二义性无法明确知道访问的是哪一个
-//	Assistant a;
-//	a._name = "peter";
-//	// 需要显示指定访问哪个父类的成员可以解决二义性问题，但是数据冗余问题无法解决
-//	a.Student::_name = "xxx";
-//	a.Teacher::_name = "yyy";
-//	return 0;
-//}
+#include <iostream>
+#include <string>
+using namespace std;
 
-/////////////////////////////////////////////////////////查看内存
-//#include <iostream>
-//using namespace std;
-//
-//class A
-//{
-//public:
-//	int _a;
-//};
-//// class B : public A
-//class B : virtual public A
-//{
-//public:
-//	int _b;
-//};
-//// class C : public A
-//class C : virtual public A
-//{
-//public:
-//	int _c;
-//};
-//class D : public B, public C
-//{
-//public:
-//	int _d;
-//};
-//int main()
-//{
-//	D d;
-//	d.B::_a = 1;
-//	d.C::_a = 2;
-//	d._b = 3;
-//	d._c = 4;
-//	d._d = 5;
-//	return 0;
-//}
+class Person
+{
+public:
+	string _name; // 姓名
+};
+
+class Student : virtual public Person
+{
+protected:
+	int _num; //学号
+};
+
+class Teacher : virtual public Person
+{
+protected:
+	int _id; // 职工编号
+};
+
+class Assistant : public Student, public Teacher
+{
+protected:
+	string _majorCourse; // 主修课程
+};
+
+int main()
+{
+	// 这样会有二义性无法明确知道访问的是哪一个
+	Assistant a;
+	a._name = "peter";
+	// 需要显示指定访问哪个父类的成员可以解决二义性问题，但是数据冗余问题无法解决
+	a.Student::_name = "xxx";
+	a.Teacher::_name = "yyy";
+	return 0;
+}
